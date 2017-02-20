7798fcb75cb8f22c94e712e627616ee1bed02b46
wuulong <wuulong@gmail.com>
2017-02-20 14:37:07 +0800

    Camera commander prototype V0.0.2 support thread

--- firmware/verification/camera_commander_prototype.py
+++ firmware/verification/camera_commander_prototype.py
@@ -3,10 +3,35 @@
 # Author:
 #    Wuulong, Created 20/02/2017
 # Arch:
-#cli
 import cmd
+import threading
+import time
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
+
+#Command socket thread
+class MonitorThread(threading.Thread):
+    def __init__(self, wait=0.01):
+        threading.Thread.__init__(self)
+        self.event = threading.Event()
+        self.wait = wait
+        self.exit = False
+
+    def set_ts(self, ts):
+        self.wait = ts
+
+    def do_function(self):
+        print("thread running...")
+        time.sleep(5)
+
+    def run(self):
+        while 1:
+            if self.exit:
+                break
+            self.do_function()
+            self.event.wait(self.wait)
+
+
 
 #FarmBot Camera Commander CLI
 class CameraCommanderCli(cmd.Cmd):
@@ -15,12 +40,33 @@ class CameraCommanderCli(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
         self.prompt = 'FCC>'
+        self.monitor_thread = None
 
 ############ cli maintain ####################
     def do_quit(self, line):
         """quit"""
+        if self.monitor_thread:
+            self.monitor_thread.exit=True
+            time.sleep(1)
         return True
 
+    def do_net_open(self,line):
+        """open socket to accept commands"""
+        if self.monitor_thread==None:
+            self.monitor_thread = MonitorThread()
+            self.monitor_thread.start()
+        else:
+            print("Command socket already exist! Don't start again")
+
+    def do_stop(self,line):
+        """ stop socket
+        stop
+        ex: stop"""
+        if self.monitor_thread:
+            self.monitor_thread.exit=True
+        time.sleep(2)
+        self.monitor_thread = None
+        
 ############ commands  ####################
     def do_r83(self,line):
         """Report software version"""
